f3fa5e9b671be81fb0a40d76157ee5405766f4cb
B_Yoda04 <gerardmatos1990@gmail.com>
2022-10-14 11:43:40 -0500

    14/10/2022

--- python_first_week.py
+++ python_first_week.py
@@ -1,22 +1,4 @@
-# x = True constante => O(1), una asignacion siempre es constante
-
-# for i in range(0, len(dataList), 2) logaritmico => O(log n), un recorrido por saltos o divisiones siempre es logaritmico
-
-# for i in dataList  lineal => O(n), los bucles de recorrido por cada item siempre son lineales
-
-# def merge_sort(data):
-#    ... merge_sort(rigth), merge_sort(left) lineal logaritmico => O(n log n), la llamada de la misma funcion dentro de esa funcion siempre es lineal logaritmico
-
-# def buble_sort(data):
-#   ... if i in range(len(data) - 1) cuadratica => O(n2), el reordenamiento constante de una lista hara que se ejecute en funcion cuadratica de la cantidad de datos que contiene por ende siempre sera cuadratica
-
-# def cubic_sort(data):
-#   for i in data:
-#       for j in data:
-#           for k in data:
-#               print(x,y,z) cubica => O(n3), la iteracion contante de for en bucle iterar la lista en funcion de la cantidad de de iteraciones de for por lo tanto sera n elevado a la m
-
-def bin_exp_mod(a, n, b):
+def bin_exp_mod(a, n, b): # => O(n), la funcion vendria siendo lineal
     """
     >>> bin_ex_mod(3, 4, 5)
     1
@@ -27,15 +9,11 @@ def bin_exp_mod(a, n, b):
     assert not (b == 0), "This cannot accept modulo that is 0"
     if n == 0: # O(n)
         # si el segundo argumento n es 0 returna 0
-        # print('first if',n)
-        print('return first if')
         return 1
 
     if n % 2 == 1: # O(n)
         # si n es impar entrara en este if volviendo a ejecutar la funcion con n - 1
-        # print('second if',n)
-        # print('return second if',(bin_exp_mod(a, n-1, b) * a) % b)
-        print('return second if')
+        # print('i = ',(bin_exp_mod(a, n-1, b) * a) % b)
         return(bin_exp_mod(a, n-1, b) * a) % b
         # al ejecutar este funcion si n es mayor que 1 entrara a ejecutar la funcion hasta llegar a r y asi sucesibamente hasta que n sea igual a 0
 
@@ -43,14 +21,30 @@ def bin_exp_mod(a, n, b):
     r = bin_exp_mod(a, n / 2, b) # O(1)
     # print('r = ',r)
     # la ejecucion de esta funcion entrara convertira a n en 1 haciendo que entre en el segundo if
-    # print('ultimo return',(r * r) % b)
-    print('ultimo return')
     return (r * r) % b
 
-bin_exp_mod(3, 4, 5) # => O(n) + O(n) + O(1) = O(n)
-print(bin_exp_mod(7, 13, 10))
+# print(bin_exp_mod(3,4,5))
+# print(bin_exp_mod(7,13,10))
+# En este caso me mareé con la ejecucion de funciones dentro de la misma funcion y no pude refactorizar
 
-# def proob():
-#     return(1*3)%5
+from collections.abc import Sequence
 
-# print(proob())
+def decimal_to_binary(no_of_variable: int, minterms: Sequence[float]) -> list[str]: # => O(n**2), la funcion vendria siendo cuadratica
+    """
+    >>> decimal_to_binary(3,[1.5])
+    ['0.00.01.5']
+    """
+    temp = [] # O(1)
+    for minterm in minterms: # O(n)
+        string = "" # O(1)
+        for i in range(no_of_variable): # O(n)
+            string = str(minterm % 2) + string # O(1)
+            print(string)
+            minterm //= 2
+        temp.append(string) # O(1)
+    return temp
+
+# O(1) + O(n) * O(1) + O(n) * O(1) + O(1) => O(n) * O(n) * O(1) => O(n**2)
+
+print(decimal_to_binary(3,[1.5]))
+# En este caso me parece que el codigo ya esta optimizado y no siento que sea necesario refactorizarlo
